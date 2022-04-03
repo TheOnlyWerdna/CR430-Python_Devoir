54a141331c05cbdb6661e1d9205b09c884971698
Andrew <Andrew>
2022-04-03 18:42:34 -0400

    Devoir 2 finished.

--- devoir2/client.py
+++ devoir2/client.py
@@ -1,45 +1,93 @@
 import socket			
 import os
-# Create a socket object
-s = socket.socket()		
+import shutil
+import sys
 
-# Define the port on which you want to connect
-port = 1339
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
-response = s.recv(1024).decode()
-print(response)
-connectedToServer = True
+available_socket = socket.socket()	
 
-while connectedToServer:
-    print("Entrez une des commandes ci-dessous: ")
-    print("TIME pour avoir le temps")
-    print("IP pour avoir l'adresse ip")
-    print("OS pour obtenir de l'information sur le systeme d'exploitation")
-    print("FICHIER pour obtenir un fichier factice")
-    print("EXIT pour quitter")
-    requestToServer = str(input("Choix: ")).lower()
+PORT = 1334
+LOCALHOST = '127.0.0.1'
+GOODBYE = "Au revoir"
 
-    if requestToServer not in ("time", "ip", "os", "fichier", "exit"):
-        print("Commande invalide!")
+def show_menu():
+    print("""Entrez une des commandes ci-dessous: 
+    TIME pour avoir le temps
+    IP pour avoir l'adresse ip
+    OS pour obtenir de l'information sur le systeme d'exploitation
+    FICHIER pour obtenir un fichier factice
+    EXIT pour quitter""")
+
+def get_chosen_option():
+    return str(input("Choix: ")).lower()
+
+def not_valid(request):
+    return request not in ("time", "ip", "os", "fichier", "exit")
+
+def show_invalid_request_message():
+    print("Commande invalide!")
+
+def is_file_download(request):
+    return request == "fichier"
+
+def get_folder_name():
+    return "./downloadsFromServer/"
+
+def already_created(folder):
+    return os.path.exists(folder)
+
+def delete_created(folder):
+    shutil.rmtree(folder)
+
+def create_destination(folder):
+    os.mkdir(folder)
+
+def send_using(available_socket, request):
+    available_socket.send(request.encode())
+
+def get_response_using(available_socket):
+    return available_socket.recv(1024).decode()
+
+def download_and_save_file_to(folder, response):
+    f = open(folder+"donneesTelecharges.txt", "a")
+    f.write(response)
+    f.close()
+
+def show_download_confirmation_to(folder):
+    print("Fichier donneesEnregistres.txt cree dans "+folder)
+
+def connect_to_server_using(available_socket):
+    available_socket.connect((LOCALHOST, PORT))
+
+def disconnect_from_server_using(available_socket):
+    available_socket.close()	
+    sys.exit(0)
+
+connect_to_server_using(available_socket)
+print(get_response_using(available_socket))
+connected_to_server = True
+
+while connected_to_server:
+    show_menu()
+    request = get_chosen_option()
+
+    if not_valid(request):
+        show_invalid_request_message()
         continue
-    elif requestToServer == "fichier":
-        folderToCreate = "./downloadsFromServer/"
-        os.mkdir(folderToCreate)
-        s.send(requestToServer.encode())
-        response = s.recv(1024).decode()
-        f = open(folderToCreate+"donneesTelecharges.txt", "a")
-        f.write(response)
-        f.close()
-        print("Fichier donneesEnregistres.txt cree dans "+folderToCreate)
+    elif is_file_download(request):
+        folder = get_folder_name()
+        if already_created(folder):
+            delete_created(folder)
+        create_destination(folder)
+        send_using(available_socket, request)
+        download_and_save_file_to(folder, get_response_using(available_socket))
+        show_download_confirmation_to(folder)
     else:
-        s.send(requestToServer.encode())
-        response = s.recv(1024).decode()
+        send_using(available_socket, request)
+        response = get_response_using(available_socket)
         print(response)
+        if response == GOODBYE:
+            connected_to_server = False
         
+disconnect_from_server_using(available_socket)    
     
-    
-# close the connection
-s.close()	
-	
--- devoir2/server.py
+++ devoir2/server.py
@@ -3,48 +3,68 @@ import sys
 import socket
 from datetime import datetime
 
-def setup_socket(port=1339):
+def setup_socket(port=1334):
     s = socket.socket()
     s.bind(('', port))
     s.listen()
     return s
 
-# Setup socket
+def send_using(client, response):
+    client.send(response)
+
+def get_file_data():
+    filename="./donnees.txt"
+    my_file = open(filename, "rb")
+    my_file_data = my_file.read()
+    my_file.close()
+    return my_file_data
+
 try:
     server_socket = setup_socket()
 except Exception as err:
     print(f"[ERROR]: Failed to setup socket.\nDetails: {err}")
     sys.exit(1)
 
-# Main loop and cleanup
 try:
     BUFFER_SIZE = 1024
+    
     while True:
         client, client_ip = server_socket.accept()
-        client.send(f"Accepted client with addr: '{client_ip[0]}'. Waiting for data...".encode())
+        client.send(f"Client connecte avec ip: '{client_ip[0]}'. En attente de requetes".encode())
+        connected_to_client = True
         
-        # Main logic part
-        data = client.recv(1024).decode()
-        print(str(data))
-        while data != 'exit':            
-            if data == 'time':
-                client.send(str(datetime.now().time()).encode())
-            elif data == 'ip':
-                client.send(client_ip[0].encode())
+        while connected_to_client: 
+            client.settimeout(20.0)
+            clientRequest = client.recv(1024).decode()
+            client.settimeout(None)
+            print(str(clientRequest))           
+            if clientRequest == 'time':
+                send_using(client, str(datetime.now().time()).encode())
+                print(str(datetime.now().time()))
+            elif clientRequest == 'ip':
+                send_using(client, client_ip[0].encode())
                 print(client_ip[0])
-            elif data == 'os':
-                client.send(platform.platform().encode())
-            elif data == 'fichier':
-                filename="./donnees.txt"
-                myfile = open(filename, "rb")
-                myfile_data = myfile.read()
-                myfile.close()
-                client.send(myfile_data)
-            data = client.recv(1024).decode()
-            print(str(data))
-except InterruptedError: # If we interrupt using CTRL+C
+            elif clientRequest == 'os':
+                send_using(client, platform.platform().encode())
+                print(platform.platform())
+            elif clientRequest == 'fichier':
+                print("Envoi du fichier complete avec succes")
+                send_using(client, get_file_data())
+            else:
+                send_using(client, "Au revoir".encode())
+                print("Fermeture du serveur.")
+                server_socket.close()  
+                sys.exit(0)
+                
+except InterruptedError: 
     print("Terminating...")
-    server_socket.close()    
+    server_socket.close()   
+    sys.exit(1) 
+except socket.timeout:
+    print("Client inactif pendant plus de 20 secondes. Fermeture du serveur.")
+    server_socket.close()  
+    sys.exit(1)
 except Exception as err:
     print(f"[ERROR]: {err}")
+    server_socket.close()  
     sys.exit(1)
